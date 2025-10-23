9b8f4ee8ef3f3a760239846377c691f00d7f7906
Akshay peherkar <95123298+Akshayp02@users.noreply.github.com>
2025-10-23 09:56:53 +0530

    Domain Setup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Services from './pages/Services';
-import RequestService from './pages/RequestService';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Services from "./pages/Services";
+import RequestService from "./pages/RequestService";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <BrowserRouter basename="/loomxTech">l̥
+    <BrowserRouter>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
